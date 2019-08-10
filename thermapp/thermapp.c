ea4fd8e405c2292305d900ebf19a97eee7bb4806
Kyle Guinn <elyk03@gmail.com>
2019-08-10 13:44:15 -0500

    Remove no-longer-needed includes

--- thermapp/thermapp.c
+++ thermapp/thermapp.c
@@ -18,16 +18,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
-
-#include <sys/types.h>
-#include <sys/stat.h>
 
 #include <time.h>
 #include <string.h>
 #include <errno.h>
-
 
 #include "thermapp.h"
 

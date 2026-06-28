fe75c9caa09d66f3e9ab2967a5c2ee6cddf4ec7e
Kyle Guinn <elyk03@gmail.com>
2026-06-28 14:45:07 -0500

    Add context manager for HTTP connection

--- calibration/get-calibration.py
+++ calibration/get-calibration.py
@@ -13,6 +13,7 @@ import json
 import re
 import shutil
 import time
+from contextlib import closing
 
 def json_request(conn, endpoint, body, headers):
 	# The app uses lowercase 'mobileservice.svc'.
@@ -101,75 +102,74 @@ if __name__ == '__main__':
 		os.mkdir(body['serialNumber'])
 	os.chdir(body['serialNumber'])
 
-	conn = http.client.HTTPConnection('api.therm-app.com')
-	#conn.set_debuglevel(1)
+	with closing(http.client.HTTPConnection('api.therm-app.com')) as conn:
+		#conn.set_debuglevel(1)
 
-	# SessionStart returns a 'sessionID' (among other things) that will be
-	# sent along with all subsequent requests and will appear in all
-	# responses.  The sessionID appears to be a datetime string in the
-	# server's localtime (+0300), down to the millisecond, and without any
-	# punctuation nor leading zeros on each date and time element.
-	print('SessionStart')
-	resp = json_request(conn, 'SessionStart', body=body, headers=headers)
-	body['sessionID'] = resp['sessionID']
+		# SessionStart returns a 'sessionID' (among other things) that will be
+		# sent along with all subsequent requests and will appear in all
+		# responses.  The sessionID appears to be a datetime string in the
+		# server's localtime (+0300), down to the millisecond, and without any
+		# punctuation nor leading zeros on each date and time element.
+		print('SessionStart')
+		resp = json_request(conn, 'SessionStart', body=body, headers=headers)
+		body['sessionID'] = resp['sessionID']
 
-	# GetFilesList returns a directory listing.
-	# Request the serialNumber directory.
-	# body['serialNumber'] is also required by the server; it is checked
-	# for validity but it doesn't need to match the requested directory.
-	body['data'] = { 'folder': body['serialNumber'] }
-	print('GetFilesList', body['serialNumber'])
-	resp = json_request(conn, 'GetFilesList', body=body, headers=headers)
-	files = resp['data']
-	# Save the listing for posterity; it contains more info than FilesList.json.
-	with open('GetFilesList.json', 'w') as f:
-		json.dump(files, f, indent="\t")
+		# GetFilesList returns a directory listing.
+		# Request the serialNumber directory.
+		# body['serialNumber'] is also required by the server; it is checked
+		# for validity but it doesn't need to match the requested directory.
+		body['data'] = { 'folder': body['serialNumber'] }
+		print('GetFilesList', body['serialNumber'])
+		resp = json_request(conn, 'GetFilesList', body=body, headers=headers)
+		files = resp['data']
+		# Save the listing for posterity; it contains more info than FilesList.json.
+		with open('GetFilesList.json', 'w') as f:
+			json.dump(files, f, indent="\t")
 
-	# Each element in the files array contains:
-	#   'id':  An integer, appears to be sequential.
-	#   'active':  A bool, unknown purpose.  Always observed as True.
-	#   'createdByUserId':  A useless UUID.
-	#   'createdDate':  A string: '/Date(x)/' where x is a timestamp.
-	#   'folder':  Same as 'folder' in our previous request.
-	#   'length':  Integer file length.
-	#   'name':  File name string.
-	#   'token':  Another UUID, used below when fetching the file.
-	#   'type':  A string.  Always observed as 'bin'.
-	#
-	# The createdDate timestamp appears to be a Unix timestamp, suffixed
-	# with milliseconds, then suffixed again with the UTC offset (+0300)
-	# to capture the location info.  This is a Microsoft .NET format.
-	re_date = re.compile('/Date\\(([0-9]+)([+-][0-9]+)?\\)/')
+		# Each element in the files array contains:
+		#   'id':  An integer, appears to be sequential.
+		#   'active':  A bool, unknown purpose.  Always observed as True.
+		#   'createdByUserId':  A useless UUID.
+		#   'createdDate':  A string: '/Date(x)/' where x is a timestamp.
+		#   'folder':  Same as 'folder' in our previous request.
+		#   'length':  Integer file length.
+		#   'name':  File name string.
+		#   'token':  Another UUID, used below when fetching the file.
+		#   'type':  A string.  Always observed as 'bin'.
+		#
+		# The createdDate timestamp appears to be a Unix timestamp, suffixed
+		# with milliseconds, then suffixed again with the UTC offset (+0300)
+		# to capture the location info.  This is a Microsoft .NET format.
+		re_date = re.compile('/Date\\(([0-9]+)([+-][0-9]+)?\\)/')
 
-	# The app requests FilesList.json first.  Perhaps it uses it to filter
-	# the files that need to be downloaded.  Instead, here we will
-	# download everything in-order.
-	#
-	# The app leaves the Accept header as application/json during these
-	# file fetches, but that seems inappropriate as the server sends it
-	# as application/octet-stream.  Change it, then change back later.
-	headers['Accept'] = 'application/octet-stream'
-	for elem in files:
-		# GetFile uses the 'token' UUID to request a file.
-		body['data'] = { 'token': elem['token'] }
-		print('GetFile', elem['name'])
-		resp = json_request(conn, 'GetFile', body=body, headers=headers)
+		# The app requests FilesList.json first.  Perhaps it uses it to filter
+		# the files that need to be downloaded.  Instead, here we will
+		# download everything in-order.
+		#
+		# The app leaves the Accept header as application/json during these
+		# file fetches, but that seems inappropriate as the server sends it
+		# as application/octet-stream.  Change it, then change back later.
+		headers['Accept'] = 'application/octet-stream'
+		for elem in files:
+			# GetFile uses the 'token' UUID to request a file.
+			body['data'] = { 'token': elem['token'] }
+			print('GetFile', elem['name'])
+			resp = json_request(conn, 'GetFile', body=body, headers=headers)
 
-		# Save the response to a file.
-		with open(elem['name'], 'wb') as f:
-			shutil.copyfileobj(resp, f)
+			# Save the response to a file.
+			with open(elem['name'], 'wb') as f:
+				shutil.copyfileobj(resp, f)
 
-		# Set its timestamp.
-		timestamp = re_date.fullmatch(elem['createdDate'])
-		if timestamp is not None:
-			ns = int(timestamp[1]) * 1000000
-			if timestamp.lastindex == 2:
-				utc_offset = time.strptime(timestamp[2], '%z').tm_gmtoff
-			os.utime(elem['name'], ns=(ns, ns))
-	headers['Accept'] = 'application/json'
+			# Set its timestamp.
+			timestamp = re_date.fullmatch(elem['createdDate'])
+			if timestamp is not None:
+				ns = int(timestamp[1]) * 1000000
+				if timestamp.lastindex == 2:
+					utc_offset = time.strptime(timestamp[2], '%z').tm_gmtoff
+				os.utime(elem['name'], ns=(ns, ns))
+		headers['Accept'] = 'application/json'
 
-	# End the session.
-	del body['data']
-	print('SessionEnd')
-	resp = json_request(conn, 'SessionEnd', body=body, headers=headers)
-	conn.close()
+		# End the session.
+		del body['data']
+		print('SessionEnd')
+		resp = json_request(conn, 'SessionEnd', body=body, headers=headers)
